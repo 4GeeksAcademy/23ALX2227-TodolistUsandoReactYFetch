8ddde7160ec2c2b3693fd79d09f2f2a14af2ac90
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-11-13 23:17:07 +0000

    Algunos cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -31,8 +31,7 @@ const home = () => {
                 const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`, {
                     body: JSON.stringify(list),
                     method: "PUT",
-                    headers: {
-                        'Content-Type': 'application/json'                   }
+                    headers: {'Content-Type': 'application/json'}
                 })
                 if (!response.ok) {
                     throw new Error("No se pudo actulizar la lista");
@@ -68,25 +67,40 @@ const home = () => {
 
         }
     }
+
+    const handlerDelete = async () => {
+        try {
+            
+        }
+
+    }
+
     useEffect(() => {
         getList();
     }, [inputValue]);
 
+
     return (
         <div className="text-center container">
-            <h3>Busca tu lista por nombre: </h3>
-            <input type="text" className="form-control" onChange={(e) => setInputValue(e.target.value)} />
-            <h3>Crear usurio:</h3>            
+
+            <h3>Crear usurio:</h3>
             <input type="text" className="form-control" onChange={(e) => setNewUserValue(e.target.value)} />
             <button className="btn btn-success mt-3" onClick={ handlerNewUser }>New User</button>
+
+            <h3>Busca tu lista por nombre: </h3>
+            <input type="text" className="form-control" onChange={(e) => setInputValue(e.target.value)} value= {inputValue} />
+
             <h3>Agregar nueva tarea: </h3>
             <input type="text" className="form-control" onChange={(e) => setNewTask(e.target.value)}/>
             <button className="btn btn-primary mt-3" onClick={ handlerClick }>Agregar Tarea</button>
+
             <ul className="list-group">
                 {
                     list.map((ele, index)=> {
-                        return <li className="list-group-item list-group-item-success mt-4" key={index}> {ele.label} </li>
-                        
+                        return <>
+                        <li className="list-group-item list-group-item-success mt-4" key={index}> {ele.label}</li>
+                        <label onClick={handlerDelete}>❌</label>
+                        </>                       
                     })
                 }                
             </ul>
