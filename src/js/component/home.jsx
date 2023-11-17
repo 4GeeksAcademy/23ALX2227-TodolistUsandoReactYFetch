cc746cc86bdb4c47d5f934ff807bef3c0e64e6be
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-11-17 01:58:00 +0000

    Retoques

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,119 +1,161 @@
 import React, { useState, useEffect } from "react";
 
 const home = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [list, setList] = useState([]);
-    const [newTask, setNewTask] = useState("");
-    const [newUserValue, setNewUserValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [list, setList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [newUserValue, setNewUserValue] = useState("");
 
-    const getList = async () => {
-        try {
-            const response = await fetch(
-                `https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`
-            );
-            if (!response.ok) {
-                throw new Error("El response get dio false ");
-            }
-            const data = await response.json();
-            setList(data);
-        } catch (err) { 
-            console.log(err)
-        }
-    };
-    const handlerClick = async () => {
-        try {
-            if (newTask) {
-                let obj = {
-                    label: newTask,
-                    done: false, 
-                }
-                setList([...list, obj])
-                const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`, {
-                    body: JSON.stringify(list),
-                    method: "PUT",
-                    headers: {'Content-Type': 'application/json'}
-                })
-                if (!response.ok) {
-                    throw new Error("No se pudo actulizar la lista");
-                }
-                const data = await response.json()
-                console.log(data)
-                
-            }else {
-                alert("Para guardar tarea debes escribir algo!")
-            }
-
-            
-        } catch(err){
-            console.log(err)
-
-        }
+  const getList = async () => {
+    try {
+      const response = await fetch(
+        `https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`
+      );
+      if (!response.ok) {
+        throw new Error("El response get dio false ");
+      }
+      const data = await response.json();
+      setList(data);
+    } catch (err) {
+      console.log(err);
     }
-    const handlerNewUser = async () => {
-        try { 
-            const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/${newUserValue}`, {
-                body: JSON.stringify([]),
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'                   }
-            }
-            )
-            const data = await response.json()
-            console.log(data)
-
-
-
-        } catch(err){
-
+  };
+  const addTaskToList = () => {
+    if (newTask) {
+      let obj = {
+        label: newTask,
+        done: false,
+      };
+      setList([...list, obj]);
+    } else {
+      alert("Para guardar tarea debes escribir algo!");
+    }
+  };
+  const removeTaskFromList = (index) => {
+    const newList = list.filter(function (item, i) {
+      return i !== index;
+    });
+    setList(newList);
+  };
+  const updateList = async (list) => {
+    try {
+      const response = await fetch(
+        `https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`,
+        {
+          body: JSON.stringify(list),
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
         }
+      );
+      if (!response.ok) {
+        throw new Error("No se pudo actulizar la lista");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
     }
+  };
+  const handlerNewUser = async () => {
+    try {
+      const response = await fetch(
+        `https://playground.4geeks.com/apis/fake/todos/user/${newUserValue}`,
+        {
+          body: JSON.stringify([]),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {}
+  };
 
-    const handlerDelete = async () => {
-        try {
-            const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/${newUserValue}`, {
-                method: "DELETE",
-                headers: { "conten-type" : "aplication/json" }
-            }
-            )
-            const data = await response.json()
-            console.log(data)
-            
-        } catch{err}
-
+  const handlerDelete = async () => {
+    try {
+      const response = await fetch(
+        `https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
+  useEffect(() => {
+    getList();
+  }, [inputValue]);
 
-    useEffect(() => {
-        getList();
-    }, [inputValue]);
+  useEffect(() => {
+    updateList(list);
+  }, [list]);
 
+  return (
+    <div className="text-center container">
+      <h3>Crear usurio:</h3>
+      <div className="row col-md-3 m-auto">
+      <input
+        type="text"
+        className="form-control"
+        onChange={(e) => setNewUserValue(e.target.value)}
+      />
+      </div>
+      <button className="btn btn-success mt-3" onClick={handlerNewUser}>
+        New User
+      </button>
 
-    return (
-        <div className="text-center container">
+      <div className="row col-md-3 m-auto">
+      <h3>Busca tu lista: </h3>
+      <input
+        type="text"
+        className="form-control"
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+      />
+      </div>
 
-            <h3>Crear usurio:</h3>
-            <input type="text" className="form-control" onChange={(e) => setNewUserValue(e.target.value)} />
-            <button className="btn btn-success mt-3" onClick={ handlerNewUser }>New User</button>
+      <div className="row col-md-5 m-auto">
+      <h3>Agregar nueva tarea: </h3>
+      <input
+        type="text"
+        className="form-control"
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      </div>
+      <button className="btn btn-primary mt-3" onClick={addTaskToList}>
+        Agregar Tarea
+      </button>
 
-            <h3>Busca tu lista por nombre: </h3>
-            <input type="text" className="form-control" onChange={(e) => setInputValue(e.target.value)} value= {inputValue} />
-
-            <h3>Agregar nueva tarea: </h3>
-            <input type="text" className="form-control" onChange={(e) => setNewTask(e.target.value)}/>
-            <button className="btn btn-primary mt-3" onClick={ handlerClick }>Agregar Tarea</button>
-
-            <ul className="list-group">
-                {
-                    list.map((ele, index)=> {
-                        return <>
-                        <li className="list-group-item list-group-item-success mt-4" key={index}> {ele.label}</li>
-                        <label onClick={handlerDelete}>❌</label>
-                        </>                       
-                    })
-                }                
-            </ul>
-        </div>
-    );
+      <ul className="list-group">
+        {list.map((ele, index) => {
+          return (
+            <li
+              className="col-md-12 list-group-item list-group-item-success mt-4"
+              key={index}
+            >
+              {" "}
+              {ele.label}{" "}
+              <button
+                className="btn btn-outline-danger mt-3"
+                onClick={() => removeTaskFromList(index)}
+              >
+                ❌
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <button className="btn btn-danger mt-3" onClick={handlerDelete}>
+        Eliminar todas la tareas
+      </button>
+    </div>
+  );
 };
 
 export default home;
